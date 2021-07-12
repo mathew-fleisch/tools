be896841ffb10eb78f23aa254e3f0e270d7f3753
mathew-fleisch <mathew.fleisch@gmail.com>
2021-07-12 15:51:50 -0700

    Refactor tag/release script to run release after docker build and pass in branch name

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 echo "Tag release, multi-arch-build and push"
+# Check dependencies
+expected="curl docker git jq"
+for expect in $expected; do
+    if ! command -v $expect > /dev/null; then
+    echo "Missing dependency: $expect"
+    exit 1
+    fi
+done
+
 echo "Check environment variables are set..."
-expected="REGISTRY_USERNAME REGISTRY_PASSWORD REGISTRY_URL REGISTRY_APPNAME GIT_TOKEN REPO_OWNER REPO_NAME"
+expected="REGISTRY_USERNAME REGISTRY_PASSWORD REGISTRY_URL REGISTRY_APPNAME GIT_TOKEN REPO_OWNER REPO_NAME REPO_BRANCH"
 for expect in $expected; do
   if [[ -z "${!expect}" ]]; then
     echo "Missing environment variable: $expect"
@@ -18,38 +27,38 @@ PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64}"
 # Fetch all tags and set most recent as a variable
 git fetch --prune --unshallow
 tag=$(git describe --tags)
-# Get commit message for selected tag
-commit_message="$(git for-each-ref refs/tags/$tag --format='%(contents)' | head -n1)"
-# Container registry credentials stored as environment variables from github secrets
+
+echo "Login to container registry"
+# Container registry credentials stored as environment variables (from github secrets when run in github-actions
+echo "$REGISTRY_PASSWORD" | docker login ${REGISTRY_URL} -u="$REGISTRY_USERNAME" --password-stdin
+# Build Dockerfile and use git tag as docker tag
+echo "Build + push multi-arch docker container"
+docker buildx build \
+  --platform $PLATFORMS \
+  -t $REGISTRY_APPNAME:$tag \
+  -t $REGISTRY_APPNAME:latest \
+  --push .
+echo "Container(s) pushed to registry!"
+
+# ---- ^ Docker Build ^ --- v Github Release v ---- #
+
 echo "Creating git release..."
-# Build json with tag + commit message
-curl_data='{"tag_name": "'$tag'", "target_commitish": "main", "name": "'$REPO_NAME'-'$tag'", "body": "'$commit_message'", "draft": false, "prerelease": false}'
-# Sanity check (does json render through jq)
-echo "Data: $curl_data"
-echo "$curl_data" | jq '.'
-# Build, print and execute curl to create a new release with the github api
-curl_post="curl -sXPOST -H \"Content-Type: application/json\" -H \"Authorization: token $GIT_TOKEN\" --data '$curl_data' https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases"
-echo "curl: "
-echo "$curl_post"
-release_response="$(eval $curl_post)"
-echo "Release Response: $release_response"
-echo "$release_response" | jq '.'
-# If the release was created, the json response will contain an id
+# Get commit message for selected tag
+commit_message="$(git for-each-ref refs/tags/${tag} --format='%(contents)' | head -n1)"
+# Create release
+release_response=$(curl -sXPOST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: token ${GIT_TOKEN}" \
+  --data "{\"tag_name\": \"${tag}\", \"target_commitish\": \"${REPO_BRANCH}\", \"name\": \"${REPO_NAME}-${tag}\", \"body\": \"${commit_message}\", \"draft\": false, \"prerelease\": false}" \
+  https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases)
+
+# If the release was created successfully, the json response will contain an id
 release_id=$(echo "$release_response" | jq -r '.id')
 echo "Release ID: $release_id"
 if [[ -n $release_id ]] && [[ "$release_id" != "null" ]]; then
-  echo "Login to container registry"
-  echo "$REGISTRY_PASSWORD" | docker login ${REGISTRY_URL} -u="$REGISTRY_USERNAME" --password-stdin
-  # Build Dockerfile and use git tag as docker tag
-  echo "Build + push multi-arch docker container"
-  docker buildx build \
-    --platform $PLATFORMS \
-    -t $REGISTRY_APPNAME:$tag \
-    -t $REGISTRY_APPNAME:latest \
-    --push .
   echo "Release complete: ${REGISTRY_URL}/${REGISTRY_APPNAME}:${tag}"
   exit 0
 else
-  echo "Could not get release id from response. Skip upload and docker push."
+  echo "Could not get id from release response. Container pushed"
   exit 1
 fi
